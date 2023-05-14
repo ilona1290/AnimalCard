6dcd6cdef53f86c5762d945af4a255b10f36b1e3
ilona1290 <ilona2000123@wp.pl>
2023-05-14 20:42:39 +0200

    Changes sizes in injection menu

--- src/pages/InjectionsMenu/InjectionsMenu.js
+++ src/pages/InjectionsMenu/InjectionsMenu.js
@@ -26,13 +26,13 @@ function InjectionsMenu(){
             <button className="header__buttons__end__btn" onClick={handleBack} style={{position: "absolute", right: "2%", top: "3.5em"}}>
                 <p>Powrót</p>
             </button>
-            <div className="menu">
+            <div className="menu injectionMenu">
                 <div className="menu__card menu__injectionCard" onClick={goToInjection1}>
-                    <img className="menu__icon" src={rabiesVaccinationsIcon} alt="calendarIcon"></img>
+                    <img className="menu__icon injection__icon" src={rabiesVaccinationsIcon} alt="calendarIcon"></img>
                     <h1>Szczepienia przeciwko wściekliźnie</h1>
                 </div>
                 <div className="menu__card menu__injectionCard" onClick={goToInjection2}>
-                    <img className="menu__icon" src={otherVaccinationsIcon} alt="treatedPetIcon"></img>
+                    <img className="menu__icon injection__icon" src={otherVaccinationsIcon} alt="treatedPetIcon"></img>
                     <h1>Szczepienia przeciwko innym chorobom zakaźnym</h1>
                 </div>
             </div>
